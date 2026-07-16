47b0987faf77f3c301fff0066338db0a35ff3f4d
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-07-16 16:06:38 +0200

    Add config file and connection to database

---
+++ src/autodoc/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from config import DB_URL
+
+engine = create_engine(DB_URL)
+
+Session = sessionmaker(engine)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
