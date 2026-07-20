0b4a909a78a900351570493ed573935335c5cd69
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-07-20 17:49:01 +0200

    Rename company router

---
+++ src/autodoc/employee/router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from ..database import get_db
+from .schema import CompanyResponse, CreateCompany
+from . import service
+
+companies = APIRouter(prefix="/companies", tags=["companies"])
+
+# Company nedpoints
+
+@companies.post("/", response_model=CompanyResponse)
+def create_company(company: CreateCompany, db: Session = Depends(get_db)):
+    return service.create_company(company, db)
