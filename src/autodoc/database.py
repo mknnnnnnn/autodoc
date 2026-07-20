93f824a1e6a9f52a42626bd65e5e4927622b9833
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-07-20 15:23:48 +0200

    Fix config imports

--- src/autodoc/database.py
+++ src/autodoc/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-from config import DB_URL
+from .config import DB_URL
 
 engine = create_engine(DB_URL)
 
