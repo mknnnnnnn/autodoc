cf8849a945155de4601772b36ee8a42d225315b7
mknnnnnnn <maksymilian.gerlach@gmail.com>
2026-07-27 14:29:38 +0200

    Add missing routers

--- src/autodoc/main.py
+++ src/autodoc/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
-from .app.router import companies as company_router
+from .app.router import (
+    companies as company_router,
+    employees as employees_router,
+    addresses as addresses_router,
+    contracts as contracts_router,
+    documents as documnets_router,
+)
 
 app = FastAPI()
 
 app.include_router(company_router)
+app.include_router(employees_router)
+app.include_router(addresses_router)
+app.include_router(contracts_router)
+app.include_router(documnets_router)
